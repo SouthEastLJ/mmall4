7aa09d5164b2607fd35a6ad8b8302fa12d9f5c1a
SouthEastLJ <982971804@qq.com>
2018-06-05 20:42:43 +0800

    project init commit

---
+++ src/main/java/com/mmall4/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.mmall4.dao;
+
+import com.mmall4.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
---
+++ src/main/java/com/mmall4/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.mmall4.dao;
+
+import com.mmall4.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
